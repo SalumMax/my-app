042447b5343e22659e8335dd2ef0bb229033bd4a
SalumMax <salumaxx@gmail.com>
2022-12-31 22:54:40 +1100

    design updates

--- src/Components/Count.jsx
+++ src/Components/Count.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Count(props) {
   return (
     <div>
-      <h3 className='quiz'>
+      <h3 className='quiz-results'>
         Correct answers: {props.countAnswers} of {props.countLength}
       </h3>
     </div>
